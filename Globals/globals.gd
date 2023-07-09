73fb33cedaeab6d51926d68ceb3347267ac95758
Akshay <sakshays.2000@gmail.com>
2023-07-09 15:37:37 +0530

    add limit on hops

--- Globals/globals.gd
+++ Globals/globals.gd
@@ -1,3 +1,3 @@
 extends Node
 
-var ammo : int = 15
+var ammo : int = 40
--- gun.gd
+++ gun.gd
@@ -7,6 +7,7 @@ signal ammo_changed
 @export var rotation_strength_high : float = 300.
 @export var hop_strength : float = 300.
 @export var max_angular_velocity : float = 10.
+@export var max_num_hops : int = 3
 
 @export var audio_speed_high : float = 1.
 @export var audio_speed_low : float = 0.8
@@ -15,6 +16,7 @@ signal ammo_changed
 
 var can_slowmo = true
 var can_shoot = true
+var num_hops = 1
 var is_touching = false
 var rotation_strength = rotation_strength_high
 # Called when the node enters the scene tree for the first time.
@@ -40,23 +42,30 @@ func _process(_delta: float) -> void:
 	elif Input.is_action_pressed("rotate_ccw"):
 		if abs(angular_velocity) < max_angular_velocity:
 			apply_torque_impulse(-rotation_strength)
-	elif Input.is_action_just_pressed("hop"):
+	elif Input.is_action_just_pressed("hop") and num_hops > 0:
 		apply_central_impulse(Vector2.UP * hop_strength)
+		num_hops -= 1
 
 	if (Input.is_action_just_pressed("rotate_ccw") or Input.is_action_just_pressed("rotate_cw")) and not is_touching: 
-		Engine.time_scale = 0.2
-		rotation_strength = rotation_strength_low
-		$Camera2D/SlowMoCanvas.slowmo_effect(0.15, 0.5)
-		AudioServer.playback_speed_scale = audio_speed_low
-
+		start_slowmo()
 	elif Input.is_action_just_released("rotate_ccw") or Input.is_action_just_released("rotate_cw"):
-		Engine.time_scale = 1.0
-		rotation_strength = rotation_strength_high
-		$Camera2D/SlowMoCanvas.slowmo_effect(0.3, 1.)
-		AudioServer.playback_speed_scale = audio_speed_high
+		end_slowmo()
 
+func start_slowmo() -> void:
+	Engine.time_scale = 0.2
+	rotation_strength = rotation_strength_low
+	$Camera2D/SlowMoCanvas.slowmo_effect(0.15, 0.5)
+	AudioServer.playback_speed_scale = audio_speed_low
+		
+func end_slowmo() -> void:
+	Engine.time_scale = 1.0
+	rotation_strength = rotation_strength_high
+	$Camera2D/SlowMoCanvas.slowmo_effect(0.3, 1.)
+	AudioServer.playback_speed_scale = audio_speed_high
+			
 func _on_body_entered(body: Node) -> void:
 	is_touching = true
+	num_hops = max_num_hops
 	
 func _on_body_exited(body: Node) -> void:
 	is_touching = false
