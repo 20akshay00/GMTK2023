b13751083e804a9886ac59df1d304047615bc68e
Akshay <sakshays.2000@gmail.com>
2023-07-09 16:14:01 +0530

    add timer

--- gun.gd
+++ gun.gd
@@ -9,10 +9,15 @@ signal ammo_changed
 @export var max_angular_velocity : float = 10.
 @export var max_num_hops : int = 3
 
+@export var slow_motion_limit : float = 1.  
+@export var slow_motion_cooldown : float = 0.2
+
 @export var audio_speed_high : float = 1.
 @export var audio_speed_low : float = 0.8
 
 @onready var bullet = $Bullet
+@onready var smtimer = $SlowMotion
+@onready var smctimer = $SlowMotionCooldown
 
 var can_slowmo = true
 var can_shoot = true
@@ -46,22 +51,29 @@ func _process(_delta: float) -> void:
 		apply_central_impulse(Vector2.UP * hop_strength)
 		num_hops -= 1
 
-	if (Input.is_action_just_pressed("rotate_ccw") or Input.is_action_just_pressed("rotate_cw")) and not is_touching: 
+	if (Input.is_action_just_pressed("rotate_ccw") or Input.is_action_just_pressed("rotate_cw")) and can_slowmo and not is_touching: 
 		start_slowmo()
-	elif Input.is_action_just_released("rotate_ccw") or Input.is_action_just_released("rotate_cw"):
+	elif (Input.is_action_just_released("rotate_ccw") or Input.is_action_just_released("rotate_cw")) and Engine.time_scale == 0.2:
 		end_slowmo()
-
+		smtimer.stop()
+	
+	print(smctimer.time_left)
+	
 func start_slowmo() -> void:
 	Engine.time_scale = 0.2
 	rotation_strength = rotation_strength_low
 	$Camera2D/SlowMoCanvas.slowmo_effect(0.15, 0.5)
 	AudioServer.playback_speed_scale = audio_speed_low
-		
+	smtimer.start(slow_motion_limit)
+	
 func end_slowmo() -> void:
 	Engine.time_scale = 1.0
 	rotation_strength = rotation_strength_high
 	$Camera2D/SlowMoCanvas.slowmo_effect(0.3, 1.)
 	AudioServer.playback_speed_scale = audio_speed_high
+	
+	can_slowmo = false
+	smctimer.start(slow_motion_cooldown)
 			
 func _on_body_entered(body: Node) -> void:
 	is_touching = true
@@ -72,3 +84,9 @@ func _on_body_exited(body: Node) -> void:
 
 func _on_shoot_timer_timeout() -> void:
 	can_shoot = true
+
+func _on_slow_motion_timeout() -> void:
+	end_slowmo()
+	
+func _on_slow_motion_cooldown_timeout() -> void:
+	can_slowmo = true
--- level.gd
+++ level.gd
@@ -5,7 +5,6 @@ func _ready() -> void:
 	$BackgroundMusic.play()
 	var tween = get_tree().create_tween()
 	tween.tween_property($BackgroundMusic, "volume_db", 0, 1)
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
